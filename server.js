2ba5ac78a07753f241e04d79d70aad52ef79bb88
BigSev15 <palat008@umn.edu>
2022-11-15 18:34:44 -0600

    fixed server logic and added some notes to db.json

--- server.js
+++ server.js
@@ -14,12 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) => {
@@ -35,7 +31,7 @@ app.post('/api/notes', (req, res) => {
         const newNote = {
             title,
             text,
-            note_id: uuid(),
+            id: uuid(),
         };
 
         readAndAppend(newNote, './db/db.json');
@@ -44,6 +40,10 @@ app.post('/api/notes', (req, res) => {
         res.error('Error in adding note');
     }
 });
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
